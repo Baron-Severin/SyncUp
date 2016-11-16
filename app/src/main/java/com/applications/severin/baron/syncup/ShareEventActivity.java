9a3b9b5677d3dc1f1558020470953a62cd80523c
Baron-Severin <severinrudie@gmail.com>
2016-11-16 15:55:01 -0800

    added share event activity layout

---
+++ app/src/main/java/com/applications/severin/baron/syncup/ShareEventActivity.java
@@ -0,0 +1,13 @@
+package com.applications.severin.baron.syncup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShareEventActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_share_event);
+  }
+}
